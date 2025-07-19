f3e068c94fbf8e4051b518539cb6b654e2f97c57
El-DoctorDot <samdantasguedes19@gmail.com>
2025-07-19 19:18:43 -0300

    file update

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,9 +1,9 @@
-const navbutton = document.querySelector('#ham-btn')
-const navbar = document.querySelector('.navigation')
+const navbutton = document.querySelector('#ham-btn');
+const navbar = document.querySelector('.navigation');
 const main = document.querySelector('main');
 
 navbutton.addEventListener('click', () => {
     navbar.classList.toggle('open');
     main.classList.toggle('main-shifted');
-    navbutton.textContent = nav.classList.contains('open') ? 'X' : '≡';
+    navbutton.textContent = navbar.classList.contains('open') ? 'X' : '☰';
 });
